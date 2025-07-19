84cfca36d1ca3b909b0ace74e08a3869c345722a
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2025-07-20 00:27:46 +0530

    specific color object detection using opencv numpy and pillow

--- util.py
+++ util.py
@@ -5,13 +5,15 @@ import numpy as np
 
 def color_limit(color):
     
-    c= np.uint8([[color]])
+    c= np.uint8([[color]])  # i/p --> 2d numpy array
     
-    hsv_color = cv2.cvtColor(c,cv2.COLOR_BGR2HSV)
+    hsv_color = cv2.cvtColor(c,cv2.COLOR_BGR2HSV)  # bgr --> hsv
     
     
-    lowerLimit = hsv_color[0][0][0] - 10,100,100
-    upperLimit = hsv_color[0][0][0] + 10,255,255
+    lowerLimit = hsv_color[0][0][0] - 10,100,100    # (hue h-10 , s = 100 , v = 100)
+    upperLimit = hsv_color[0][0][0] + 10,255,255    # (hue h+10 , s = 255 , v = 255)
+    
+    # converting values into numpy array...........inRange() func works on arrays 
     
     lowerLimit = np.array(lowerLimit,dtype=np.uint8)
     upperLimit = np.array(upperLimit,dtype=np.uint8)
